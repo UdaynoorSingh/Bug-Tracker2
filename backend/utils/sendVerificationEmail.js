5ccba85a90f3b04bdb52d5042b85762f3a9c3a39
UdaynoorSingh <garsh5444@gmail.com>
2025-06-19 09:38:20 +0530

    Initial commit

--- backend/utils/sendVerificationEmail.js
+++ backend/utils/sendVerificationEmail.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const sendVerificationEmail = async (to, token) => {
-  const baseUrl = 'bug-tracker2.vercel.app';
+  const baseUrl = 'https://bug-tracker2-1.onrender.com';
   const verificationLink = `${baseUrl}/api/auth/verify-email?token=${token}`;
 
   const transporter = nodemailer.createTransport({
